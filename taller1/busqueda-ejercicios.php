cb4179eb7ce0247ff9ab052574aca6f840ab6bc0
mafebsalazar <mfbuitrago67390@umanizales.edu.co>
2024-03-30 00:12:45 -0500

    parametrizado

--- taller1/busqueda-ejercicios.php
+++ taller1/busqueda-ejercicios.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -52,8 +51,20 @@
         $dbpassword = "mafe1234";
 
         $conn = new PDO("mysql:host=localhost;port=3310;dbname=gym", $dbuser, $dbpassword);
-        $consultaSQL = $conn->prepare("SELECT ejerciciosRealizados, tipoEntrenamiento,duracionEntrenamiento FROM entrenamientos WHERE tipoEntrenamiento = '$tipoEntrenamiento'");
-        $consultaSQL->execute();
+
+        //VUL
+        //$consultaSQL = $conn->prepare("SELECT ejerciciosRealizados, tipoEntrenamiento,duracionEntrenamiento FROM entrenamientos WHERE tipoEntrenamiento = '$tipoEntrenamiento'");
+        //$consultaSQL->execute();
+
+        //SEG
+       $sentencia= "SELECT ejerciciosRealizados, tipoEntrenamiento,duracionEntrenamiento FROM entrenamientos WHERE tipoEntrenamiento = :tipoEntrenamiento;";
+
+       $consultaSQL = $conn->prepare($sentencia);
+
+       $consultaSQL->execute(array(
+         ':tipoEntrenamiento' => $tipoEntrenamiento,
+
+        ) );
     ?>
     <table border="1">
         <tr>
@@ -76,82 +87,3 @@
     </table>
 </body>
 </html>
-=======
-<!DOCTYPE html>
-<html>
-<head>
-    <title>busquedaEjercicios</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        th, td {
-            padding: 8px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-
-        tr:nth-child(even) {
-            background-color: #f2f2f2; /* Color de fondo para filas pares */
-        }
-
-        h1 {
-            color: #333; /* Color del título */
-        }
-
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 20px;
-            background-color: #fff; /* Color de fondo de la página */
-        }
-
-        input[type="text"], input[type="submit"] {
-            padding: 5px;
-            margin-bottom: 10px;
-        }
-    </style>
-</head>
-<body>
-    <h1>Busque el ejercicio</h1>
-    <form action="" method="GET">
-        entrenamientos a buscar:
-        <input type="text" name="tipoEntrenamiento">
-        <input type="submit" value="Buscar">
-    </form>
-    <?php 
-    if(isset($_GET["tipoEntrenamiento"])) {
-        $tipoEntrenamiento = $_GET["tipoEntrenamiento"];
-        echo "Busqueda por $tipoEntrenamiento";
-
-        $dbuser = "mafe";
-        $dbpassword = "mafe1234";
-
-        $conn = new PDO("mysql:host=localhost;port=3310;dbname=gym", $dbuser, $dbpassword);
-        $consultaSQL = $conn->prepare("SELECT ejerciciosRealizados, tipoEntrenamiento,duracionEntrenamiento FROM entrenamientos WHERE tipoEntrenamiento = '$tipoEntrenamiento'");
-        $consultaSQL->execute();
-    ?>
-    <table border="1">
-        <tr>
-            <th>Ejercicios Realizados</th>
-            <th>Tipo Entrenamiento</th>
-            <th>Duración Entrenamiento</th>
-        </tr>
-    <?php
-        while ($row = $consultaSQL->fetch(PDO::FETCH_ASSOC)) {
-    ?>
-        <tr>
-            <td><?php echo $row["ejerciciosRealizados"] ?></td>
-            <td><?php echo $row["tipoEntrenamiento"] ?></td>
-            <td><?php echo $row["duracionEntrenamiento"] ?></td>
-        </tr>
-    <?php
-        }
-    }
-    ?>
-    </table>
-</body>
-</html>
->>>>>>> 96ff937db38780676ed7fadfcb5ea6ad8225fb46
--- taller1/busqueda-pagos.php
+++ taller1/busqueda-pagos.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,7 +10,7 @@
 
         th, td {
             padding: 8px;
-            text-align: left;
+            text-align: left; 
             border-bottom: 1px solid #ddd;
         }
 
@@ -55,10 +54,21 @@
             $dbpassword = "mafe1234";
 
                 $conn = new PDO("mysql:host=localhost;port=3310;dbname=gym", $dbuser, $dbpassword);
-                $consultaSQL = $conn->prepare("SELECT metodoPago, fechaPago, email FROM pago WHERE fechaPago= '$fechaPago' ");
-                $consultaSQL->execute();
+                //VUL 
+                //$consultaSQL = $conn->prepare("SELECT metodoPago, fechaPago, email FROM pago WHERE fechaPago= '$fechaPago' ");
+                //$consultaSQL->execute();
         
+                //SEG
+                $sentencia= "SELECT metodoPago, fechaPago, email FROM pago WHERE fechaPago = :fechaPago;";
+
+                $consultaSQL = $conn->prepare($sentencia);
+
+                $consultaSQL->execute(array(
+                  ':fechaPago' => $fechaPago,
+
+                ) );
     ?>
+
 
                 
 
@@ -84,90 +94,3 @@
     </table>
 </body>
 </html>
-=======
-<!DOCTYPE html>
-<html>
-<head>
-    <title>busquedaPagos</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        th, td {
-            padding: 8px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-
-        tr:nth-child(even) {
-            background-color: #f2f2f2; /* Color de fondo para filas pares */
-        }
-
-        h1 {
-            color: #333; /* Color del título */
-        }
-
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 20px;
-            background-color: #fff; /* Color de fondo de la página */
-        }
-
-        input[type="text"], input[type="submit"] {
-            padding: 5px;
-            margin-bottom: 10px;
-        }
-    </style>
-</head>
-<body>
-    <h1>Pago a buscar</h1>
-    <form action="" method="GET">
-
-        FECHA : 
-        <input type="date" name="fechaPago">
-        <input type="submit" value="Buscar">
-    
-    </form>
-    <?php 
-
-    if(isset($_GET["fechaPago"])) {
-        $fechaPago = $_GET["fechaPago"]; 
-        echo "Busqueda por $fechaPago";
-      
-            $dbuser = "mafe";
-            $dbpassword = "mafe1234";
-
-                $conn = new PDO("mysql:host=localhost;port=3310;dbname=gym", $dbuser, $dbpassword);
-                $consultaSQL = $conn->prepare("SELECT metodoPago, fechaPago, email FROM pago WHERE fechaPago= '$fechaPago' ");
-                $consultaSQL->execute();
-        
-    ?>
-
-                
-
-    
-    <table border="1">
-        <tr>
-            <th>METODO DE PAGO</th>
-            <th>FECHA DE PAGO</th>
-            <th>CORREO ELECTRONICO</th>
-        </tr>
-    <?php
-        while ($row = $consultaSQL->fetch(PDO::FETCH_ASSOC)) {
-    ?>
-        <tr>
-            <td><?php echo $row["metodoPago"] ?></td>
-            <td><?php echo $row["fechaPago"] ?></td>
-            <td><?php echo $row["email"] ?></td>
-        </tr>
-    <?php
-        }
-    }
-    ?>
-    </table>
-</body>
-</html>
->>>>>>> 96ff937db38780676ed7fadfcb5ea6ad8225fb46
